998edef9a41e2bc80055d257325e679ef08f0810
leylaozdemirIT <mucuku1402@gmail.com>
2020-05-01 10:21:44 -0400

    day32_Predicate

--- src/day31_BulkOperations/RemoveDuplicates.java
+++ src/day31_BulkOperations/RemoveDuplicates.java
@@ -9,12 +9,27 @@ public class RemoveDuplicates {
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll (Arrays.asList(1,1,2,2,3,3));
 
-        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>(); // 1,2,3
 
         for (Integer each : list){ // each: 1,1,2,2,3,3
             if (!result.contains(each)){
                 result.add(each);
             }
         }
+        System.out.println(result);
+
+        System.out.println("==============================");
+
+        Integer [] arr1 = {1,1,2,2,3,3};
+
+        ArrayList<Integer> nonDuplicates = new ArrayList<>();
+
+        for (int i=0; i<= arr1.length-1; i++){
+            if ( !nonDuplicates.contains(arr1[i] ) ){
+                nonDuplicates.add(arr1[i]);
+            }
+        }
+        System.out.println(nonDuplicates);
+
     }
 }
